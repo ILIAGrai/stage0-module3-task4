6f56062c6220e7c2100eda28d85a5d711ea79203
Ilucha <pozta_01@mail.ru>
2023-06-27 22:39:41 +0300

    Ave

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -2,5 +2,8 @@ package school.mjc.stage0.module3.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        int a;
+        int b;
+        System.out.println("(9a\u00B2 + 5b + 2 + a -7)*((a+b-4ab)/2)");
     }
 }
